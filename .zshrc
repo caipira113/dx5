ea79d0743daf42df962051413f1f39dafefe1645
caipira113 <caipira113@ocellaris.dev>
2022-08-13 05:10:19 +0900

    ready

---
+++ .zshrc
@@ -0,0 +1,77 @@
+# zplug (aur)
+source ~/.zplug/init.zsh
+
+# Colorize older terminal apps (like man)
+# Start blinking
+export LESS_TERMCAP_mb=$(tput bold; tput setaf 2) # green
+# Start bold
+export LESS_TERMCAP_md=$(tput bold; tput setaf 2) # green
+# Start stand out
+export LESS_TERMCAP_so=$(tput bold; tput setaf 3) # yellow
+# End standout
+export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
+# Start underline
+export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 1) # red
+# End bold, blinking, standout, underline
+export LESS_TERMCAP_me=$(tput sgr0)
+
+# aliases
+alias ls='exa --long --header --icons --sort=type'
+alias l="ls"
+alias ll="ls -al"
+alias tree='exa --tree --level 3'
+alias sdn="sudo shutdown -h now" # Quick shutdown
+alias cp="cp -i" # Confirm before overwriting something
+alias pkg="yay -Q | fzf"
+alias yt='yt-dlp --add-metadata -i'
+alias reset='reset ; source ~/.zshrc'
+alias cls='clear'
+
+# load the good parts from oh-my-zsh
+# zsh auto completion
+zplug "lib/completion",      from:oh-my-zsh
+# setups up histoyr
+zplug "lib/history",         from:oh-my-zsh
+# Color highlighting in terminal
+zplug "zdharma/fast-syntax-highlighting"
+# Auto suggests commands based on history
+zplug "zsh-users/zsh-autosuggestions"
+
+# check plugins
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+        if read -q; then
+        echo; zplug install
+    fi
+fi
+
+# load plugins
+zplug load
+
+# run pfetch if terminal is interactive (https://github.com/dylanaraps/pfetch)
+[ -z "$PS1" ] || pfetch
+
+# typewritten
+source ~/.npm/global/lib/node_modules/typewritten/typewritten.zsh
+
+# npm global
+export PATH=~/.npm/global/bin:$PATH
+
+# gcloud
+export CLOUDSDK_PYTHON=$(which python3)
+
+# nvm
+source /usr/share/nvm/init-nvm.sh
+
+# Set typewritten ZSH as a prompt
+autoload -U promptinit; promptinit
+prompt typewritten
+fpath=($fpath "~/.zfunctions")
+
+# Keybindings
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[5~" beginning-of-history
+bindkey "^[[6~" end-of-history
+bindkey "^[[3~" delete-char
+bindkey "^[[2~" quoted-insert
---
+++ install.sh
@@ -0,0 +1,84 @@
+function syncFile() {
+    local sourceFile="$1"
+    ln -sf "$CWD/${sourceFile}" "$HOME/${sourceFile}"
+}
+
+function doSync() {
+    syncFile ".gitconfig"
+    syncFile ".zshenv"
+    return 0
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  doSync
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+      doSync
+    else
+        printf "Skipped.\n\n"
+  fi
+fi
+unset doSync
+
+if [ -f /etc/debian_version ]; then
+    # Update
+    curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+    sudo apt update; sudo apt full-upgrade -y; sudo apt autoremove -y
+    # Install packages
+    sudo apt install -y git zsh exa npm
+    # Install pfetch
+    git clone https://github.com/dylanaraps/pfetch.git
+    sudo install pfetch/pfetch /usr/local/bin/
+    ls -l /usr/local/bin/pfetch
+    rm -rf pfetch
+    # Change shell to zsh
+    sudo chsh -s /bin/zsh "$USER"
+elif [ -f /etc/arch-release ]; then
+    sudo pacman -Syu --noconfirm
+    # Install yay
+    sudo pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git
+    cd yay && makepkg -sic && cd .. && rm -rf yay
+    # Install pfetch
+    yay -S --noconfirm pfetch
+    # Install packages
+    sudo pacman -S --noconfirm zsh exa npm
+    # Change shell to zsh
+    sudo chsh -s /bin/zsh "$USER"
+fi
+
+# Install zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+# Change npm global config
+mkdir -p ~/.npm/global
+export PATH=~/.npm/global/bin:$PATH
+npm config set prefix ~/.npm/global
+
+# Install typewritten
+npm install -g typewritten
+
+# check nodejs installation
+if ! which node > /dev/null; then
+    read -p "Install nodejs? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        if [ -f /etc/debian_version ]; then
+            sudo apt-get install -y nodejs
+        elif [ -f /etc/arch-release ]; then
+            sudo pacman -S --noconfirm nodejs
+        fi
+    fi
+fi
+
+# check nvm installation
+if [ -d ~/.nvm ]; then
+    read -p "Install nvm? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    fi
+fi
+
+echo ""
+echo "done."
